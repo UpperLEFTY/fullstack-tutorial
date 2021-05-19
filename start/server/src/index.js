04ea20f5ee385ceca8c3a091467861f8375255e0
Essence Alexander <upperlefty@zoho.eu>
2021-05-19 12:50:44 -0700

    new server
    sample server for the app

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -3,3 +3,11 @@ require('dotenv').config();
  const typeDefs = require('./schema');
 
  const server = new ApolloServer({ typeDefs });
+
+ server.listen().then(() => {
+    console.log(`
+      Server is running!
+      Listening on port 4000
+      Explore at https://studio.apollographql.com/dev
+    `);
+  });
